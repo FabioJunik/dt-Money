57969d5a8d12da98fdcb191a838d6e35646170b6
VyaliMuteka <fabiojunik@gmail.com>
2022-06-11 15:19:39 +0100

    Configurando Miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import {useEffect} from 'react';
 import { Container } from "./styles";
 
 export function TransactionTable (){
+    useEffect(()=>{
+        fetch('http://localhost:3000/api/transactions')
+            .then(response=>response.json())
+            .then(json => console.log(json));
+    },[]);
+    
     return (
         <Container>
             <table>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createServer} from 'miragejs';
 import {App} from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', ()=>{
+      return [
+        {
+          id:1,
+          title:'Transation 1',
+          amount: 2000,
+          category:'Food',
+          createAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
