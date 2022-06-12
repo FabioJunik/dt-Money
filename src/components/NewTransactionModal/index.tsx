2b9ef657d093175668212da9ddd4b737f38d0246
VyaliMuteka <fabiojunik@gmail.com>
2022-06-12 07:12:30 +0100

    Criando botões de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
-import {Container} from './styles';
+import {Container, TransactionTypeContainer} from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 Modal.setAppElement('#root');
 
@@ -38,6 +40,22 @@ export function NewTransactionModal ({isOpen,onRequestClose}:NewTransactionModal
           type='number'
           placeholder='Valor'
         />
+
+        <TransactionTypeContainer>
+          <button
+            type='button'            
+          >
+            <img src={incomeImg} alt='Entrada'/>      
+            <span>Entrada</span>      
+          </button>
+
+          <button
+            type='button'            
+          >
+            <img src={outcomeImg} alt='Saída'/>      
+            <span>Saída</span>      
+          </button>
+        </TransactionTypeContainer>
 
         <input
           type='text'
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {darken} from 'polished';
 
 export const Container = styled.form`
 
@@ -48,4 +49,42 @@ export const Container = styled.form`
 		}
 	}
 
+`;
+
+
+export const TransactionTypeContainer = styled.div`
+	margin:1rem 0;
+	display:grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 0.5rem;
+	
+	button{
+		height:4rem;
+		border:1px solid #d7d7d7;
+		border-radius:0.25rem;
+
+		background-color: transparent;
+
+		display:flex;
+		align-items:center;
+		justify-content:center;
+
+		transition: 0.2s;
+
+		&:hover{
+			border-color:${darken(0.1, '#d7d7d7')};
+		}
+
+		img{
+			width:20px;
+			height:20px;
+		}
+
+		span{
+			display:inline-block;
+			margin-left:1rem;
+			font-size:1rem;
+			color:var(--text-title);
+		}
+	}
 `;
