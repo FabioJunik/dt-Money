5ff01cc668d9221665e685b1a70b6418be6f457c
FabioJunik <fabiojunik@gmail.com>
2022-06-15 17:26:14 +0100

    Inserindo transações na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -6,6 +6,7 @@ import {Container, TransactionTypeContainer, RadioBox} from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 
 Modal.setAppElement('#root');
 
@@ -22,12 +23,15 @@ export function NewTransactionModal ({isOpen,onRequestClose}:NewTransactionModal
 
   function handleCreateNewTransaction (event: FormEvent){
     event.preventDefault();
-    console.log({
+    
+    const data = {
       title,
       value,
       category,
       type  
-    });
+    };
+
+    api.post('/transactions',data);
   }
 
   return(
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createServer} from 'miragejs';
+import {createServer, Model} from 'miragejs';
 import {App} from './App';
 
 createServer({
+  
+  models:{
+    transaction: Model
+  },
   routes(){
     this.namespace = 'api';
     this.get('/transactions', ()=>{
-      return [
-        {
-          id:1,
-          title:'Transation 1',
-          amount: 2000,
-          category:'Food',
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
     })
+    this.post('/transactions',(schema, request)=>{
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction',data);
+    });
   }
 });
 
