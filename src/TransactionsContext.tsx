677aece416a29c56dad9b27e8e651b5340709db9
FabioJunik <fabiojunik@gmail.com>
2022-06-16 06:56:27 +0100

    Inserindo as novas transações na tabla

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ interface TransactionsProviderProps{
 
 interface TransationsContextData {
     transactions: Transaction[],
-    createTransaction: (transation: TransactionInput) => void
+    createTransaction: (transation: TransactionInput) => Promise<void>
 }
 
 export const TransactionsContext = createContext<TransationsContextData> 
@@ -32,11 +32,15 @@ export function TransactionsProvider({children}: TransactionsProviderProps){
             .then(response => setTransactions(response.data.transactions));
     },[]);
 
-    function createTransaction(transaction: TransactionInput){
-        
-      
-          api.post('/transactions',transaction);
-    }
+    async function createTransaction(transactionInput: TransactionInput){
+          const response = await api.post('/transactions',{
+            ...transactionInput,
+            createdAt: new Date()
+        });
+          const {transaction} = await response.data;
+
+          setTransactions([...transactions, transaction]);
+    }   
 
     return (
         <TransactionsContext.Provider value={{transactions, createTransaction}}>
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,12 @@
 import {FormEvent, useState, useContext} from 'react';
 import Modal from 'react-modal';
 
+import { TransactionsContext } from '../../TransactionsContext';
 import {Container, TransactionTypeContainer, RadioBox} from './styles'; 
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { api } from '../../services/api';
-import { TransactionsContext } from '../../TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -24,14 +23,21 @@ export function NewTransactionModal ({isOpen,onRequestClose}:NewTransactionModal
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction (event: FormEvent){
+  async function handleCreateNewTransaction (event: FormEvent){
     event.preventDefault();
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     })
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+
+    onRequestClose();
   }
 
   return(
